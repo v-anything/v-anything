044d34ad5a16c4319db3621a71d5c34ea91ca33f
summergua <1363106651@qq.com>
2024-06-17 22:33:22 +0800

    feat: monorepo初次尝试成功！

---
+++ directives/src/index.ts
@@ -0,0 +1,3 @@
+import { sum } from "./util";
+
+export { sum };
--- play/src/main.ts
+++ play/src/main.ts
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { sum } from "@sudive/directives";
 
-createApp(App).mount('#app')
+console.log(sum(1, 2));
+createApp(App).mount("#app");
