985b23e3f31ed762d145b2ac449ad1f2458c7321
summergua <1363106651@qq.com>
2024-06-18 21:39:53 +0800

    feat: new feature: v-hovertime

--- directives/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { sum } from "./util";
-
-export { sum };
--- play/src/main.ts
+++ play/src/main.ts
@@ -1,7 +1,5 @@
 import { createApp } from "vue";
-import "./style.css";
 import App from "./App.vue";
-import { sum } from "@sudive/directives";
+import "./style.css";
 
-console.log(sum(1, 2));
 createApp(App).mount("#app");
