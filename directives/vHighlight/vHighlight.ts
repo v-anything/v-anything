143c28f4769952ad943597b4dcea6425814ad3be
summergua <1363106651@qq.com>
2024-07-01 23:29:29 +0800

    refactor: better codes

--- directives/vHighlight/vHighlight.ts
+++ directives/vHighlight/vHighlight.ts
@@ -1,80 +1,6 @@
 import type { ObjectDirective } from "vue";
-import type { ElementWithHighlighter, Binding, IHighlightUtil } from "./types";
-
-class HighlightUtil implements IHighlightUtil {
-  private static instance: HighlightUtil;
-  highlights: Highlight[];
-  isRainbow: boolean;
-
-  constructor() {
-    this.highlights = [];
-    this.initCSSHighlights();
-  }
-
-  // singleton
-  public static getInstance(): HighlightUtil {
-    if (!apiAvailableHandler()) return;
-    if (!HighlightUtil.instance) {
-      HighlightUtil.instance = new HighlightUtil();
-    }
-    return HighlightUtil.instance;
-  }
-
-  private initCSSHighlights() {
-    const highlightInstance = new Highlight();
-    this.highlights.push(highlightInstance);
-    CSS.highlights.set(`default-highlight`, highlightInstance);
-
-    for (let i = 0; i < 7; i++) {
-      const highlightInstance = new Highlight();
-      this.highlights.push(highlightInstance);
-      CSS.highlights.set(`rainbow-color-${i}`, highlightInstance);
-    }
-  }
-
-  public generateHighlights = (
-    el: HTMLElement,
-    binding: { value: Binding }
-  ) => {
-    const isRainbow = binding.value.rainbow || false;
-    // get text nodes
-    const treeWalker = document.createTreeWalker(el, NodeFilter.SHOW_TEXT);
-    const allTextNodes: Node[] = [];
-    let currentNode = treeWalker.nextNode();
-    while (currentNode) {
-      allTextNodes.push(currentNode);
-      currentNode = treeWalker.nextNode();
-    }
-
-    // set color for each keyword
-    const keywords = binding.value.keywords;
-    for (let i = 0; i < keywords.length; i++) {
-      const str = keywords[i];
-      allTextNodes
-        .map((node) => ({
-          node,
-          text: node.textContent.toLocaleLowerCase(),
-        }))
-        .map(({ node, text }) => {
-          const indices = [];
-          let startPos = 0;
-          let i = 0;
-          while (startPos < text.length) {
-            i++;
-            const index = text.indexOf(str, startPos);
-            if (index === -1) break;
-            indices.push(index);
-            startPos = index + str.length;
-
-            const range = new Range();
-            range.setStart(node, index);
-            range.setEnd(node, startPos);
-            this.highlights[isRainbow ? (i % 7) + 1 : 0].add(range);
-          }
-        });
-    }
-  };
-}
+import type { ElementWithHighlighter, Binding } from "./types";
+import { HighlightUtil } from "./HighlightUtil";
 
 export const vHighlight: ObjectDirective<ElementWithHighlighter, Binding> = {
   created(el) {
@@ -87,12 +13,4 @@ export const vHighlight: ObjectDirective<ElementWithHighlighter, Binding> = {
   updated(el, binding) {
     el.$highlighter.generateHighlights(el, binding);
   },
-};
-
-const apiAvailableHandler = () => {
-  if (!CSS.highlights) {
-    console.warn("browser does not support CSS custom highlight API");
-    return false;
-  }
-  return true;
 };
