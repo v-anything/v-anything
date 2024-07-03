8f09fabfcad4ec495f25b16737ecdfed3435bbe2
shushilong <shushilong@xiaohongshu.com>
2024-07-03 13:12:54 +0800

    feat: add vitepress

--- play/src/main.ts
+++ play/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import { sum } from "@sudive/directives";
+import { sum } from "@v-anything/directives";
 
 console.log(sum(1, 2));
 createApp(App).mount("#app");
