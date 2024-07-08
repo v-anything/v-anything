5cd2d86ebb078cc56a0e7ee6cdfbec960ad4d23c
summergua <1363106651@qq.com>
2024-07-08 21:27:21 +0800

    feat: rollup declaration files

--- directives/rollup.config.js
+++ directives/rollup.config.js
@@ -1,18 +1,45 @@
 import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
+import { dts } from 'rollup-plugin-dts'
 
-export default {
-  input: './index.ts',
-  output: {
-    file: 'dist/index.js',
-    format: 'es',
+const input = './src/index.ts'
+const plugins = [
+  resolve({
+    browser: true,
+  }),
+  typescript({
+    tsconfig: './tsconfig.json',
+  }),
+]
+
+export default [
+  {
+    input,
+    output: {
+      name: 'v-anything',
+      file: 'dist/index.js',
+      format: 'es',
+    },
+    plugins,
   },
-  plugins: [
-    resolve({
-      browser: true,
-    }),
-    typescript({
-      tsconfig: './tsconfig.json',
-    }),
-  ],
-}
+  {
+    input,
+    output: {
+      name: 'vAnything',
+      file: 'dist/index.iife.js',
+      format: 'iife',
+    },
+    plugins,
+  },
+  {
+    input: './dist/types/index.d.ts',
+    output: {
+      file: 'dist/index.d.ts',
+      format: 'es',
+    },
+    plugins: [
+      ...plugins,
+      dts(),
+    ],
+  },
+]
